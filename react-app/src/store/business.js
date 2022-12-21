5dd3381f266857893fe27740d75edd8d31bd4963
Andre <andre.christian.silva@gmail.com>
2022-12-20 21:54:12 -0800

    Made Business Actions in Redux Store.

--- react-app/src/store/business.js
+++ react-app/src/store/business.js
@@ -25,6 +25,50 @@ const deleteBusiness = (bizId) => ({
     payload: bizId
 })
 
+export const getBusinesses = () => async (dispatch) => {
+    const response = await csrfFetch('/api/biz')
+    if (response.ok) {
+        const biz = await response.json()
+        dispatch(loadBusinesses(biz))
+    }
+}
+
+export const createBusiness = (bizData) => async (dispatch) => {
+    const response = await csrfFetch('/api/biz', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bizData),
+    });
+
+    if (response.ok) {
+        const biz = await response.json()
+        dispatch(addBusiness(biz))
+    }
+}
+
+export const editBusiness = (bizData) => async (dispatch) => {
+    const response = await csrfFetch(`/api/biz/${bizData.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bizData),
+    });
+    if (response.ok) {
+        const biz = await response.json()
+        dispatch(updateBusiness(biz))
+    }
+}
+
+export const removeBusiness = (bizData) => async (dispatch) => {
+    const response = await csrfFetch(`/api/biz/${bizData.id}`, {
+        method: "DELETE",
+    })
+    await response.json()
+    dispatch(deleteBusiness(bizData.id))
+}
 export default function reducer(state = {}, action) {
     switch (action.type) {
 
