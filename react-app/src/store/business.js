5e7416651a36b7c4408d7edf81b9962e4e5b89f9
geoffreyjiang <geoffreychjiang@gmail.com>
2022-12-21 21:36:52 -0800

    menu thunks

--- react-app/src/store/business.js
+++ react-app/src/store/business.js
@@ -1,66 +1,66 @@
-const LOAD_BUSINESSES = 'biz/LOAD_BUSINESSES'
-const GET_MY_BUSINESS = 'biz/GET_MY_BUSINESS'
-const ADD_BUSINESS = 'biz/ADD_BUSINESS'
-const UPDATE_BUSINESS = 'biz/UPDATE_BUSINESS'
-const DELETE_BUSINESS = 'biz/DELETE_BUSINESS'
-const GET_BUSINESS_BY_ID = 'biz/GET_BUSINESS_BY_ID'
+const LOAD_BUSINESSES = "biz/LOAD_BUSINESSES";
+const GET_MY_BUSINESS = "biz/GET_MY_BUSINESS";
+const ADD_BUSINESS = "biz/ADD_BUSINESS";
+const UPDATE_BUSINESS = "biz/UPDATE_BUSINESS";
+const DELETE_BUSINESS = "biz/DELETE_BUSINESS";
+const GET_BUSINESS_BY_ID = "biz/GET_BUSINESS_BY_ID";
 
 const loadBusinesses = (biz) => ({
     type: LOAD_BUSINESSES,
-    biz
-})
+    biz,
+});
 
 const loadBusinessbyId = (biz) => ({
     type: GET_BUSINESS_BY_ID,
-    biz
-})
+    biz,
+});
 
 const loadMyBusiness = (biz) => ({
     type: GET_MY_BUSINESS,
-    biz
-})
+    biz,
+});
 
 const addBusiness = (biz) => ({
     type: ADD_BUSINESS,
-    biz
-})
+    biz,
+});
 
 const updateBusiness = (biz) => ({
     type: UPDATE_BUSINESS,
-    biz
-})
+    biz,
+});
 
 const deleteBusiness = (bizId) => ({
     type: DELETE_BUSINESS,
-    bizId
-})
+    bizId,
+});
 
 export const getBusinesses = () => async (dispatch) => {
-    const response = await csrfFetch('/api/biz')
+    const response = await csrfFetch("/api/biz");
     if (response.ok) {
-        const biz = await response.json()
-        dispatch(loadBusinesses(biz))
+        const biz = await response.json();
+        dispatch(loadBusinesses(biz));
     }
-}
+};
 
 export const getBusinessId = (bizId) => async (dispatch) => {
-    const response = await csrfFetch(`/api/biz/${bizId}`)
+    const response = await csrfFetch(`/api/biz/${bizId}`);
     if (response.ok) {
-        const biz = await response.json()
-        dispatch(loadBusinessbyId(biz))
+        const biz = await response.json();
+        dispatch(loadBusinessbyId(biz));
     }
-}
+};
 
 export const getMyBusiness = () => async (dispatch) => {
-    const response = await csrfFetch(`/api/biz/current`)
+    const response = await csrfFetch(`/api/biz/current`);
     if (response.ok) {
-        const biz = await response.json()
-        dispatch(loadMyBusiness(biz))
+        const biz = await response.json();
+        dispatch(loadMyBusiness(biz));
     }
-}
+};
 
 export const createBusiness = (bizData) => async (dispatch) => {
-    const response = await csrfFetch('/api/biz', {
+    const response = await csrfFetch("/api/biz", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -69,10 +69,10 @@ export const createBusiness = (bizData) => async (dispatch) => {
     });
 
     if (response.ok) {
-        const biz = await response.json()
-        dispatch(addBusiness(biz))
+        const biz = await response.json();
+        dispatch(addBusiness(biz));
     }
-}
+};
 
 export const editBusiness = (bizData) => async (dispatch) => {
     const response = await csrfFetch(`/api/biz/${bizData.id}`, {
@@ -83,36 +83,36 @@ export const editBusiness = (bizData) => async (dispatch) => {
         body: JSON.stringify(bizData),
     });
     if (response.ok) {
-        const biz = await response.json()
-        dispatch(updateBusiness(biz))
+        const biz = await response.json();
+        dispatch(updateBusiness(biz));
     }
-}
+};
 
 export const removeBusiness = (bizData) => async (dispatch) => {
     const response = await csrfFetch(`/api/biz/${bizData.id}`, {
         method: "DELETE",
-    })
-    await response.json()
-    dispatch(deleteBusiness(bizData.id))
-}
+    });
+    await response.json();
+    dispatch(deleteBusiness(bizData.id));
+};
 
 export default function reducer(state = {}, action) {
-    const newState = { ...state }
+    const newState = { ...state };
     switch (action.type) {
         case LOAD_BUSINESSES:
-            return action.biz
+            return action.biz;
         case GET_BUSINESS_BY_ID:
-            return action.biz
+            return action.biz;
         case GET_MY_BUSINESS:
-            return action.biz
+            return action.biz;
         case ADD_BUSINESS:
-            newState[action.biz.id] = action.biz
-            return newState
+            newState[action.biz.id] = action.biz;
+            return newState;
         case UPDATE_BUSINESS:
-            newState[action.biz.id] = action.biz
-            return newState
+            newState[action.biz.id] = action.biz;
+            return newState;
         case DELETE_BUSINESS:
-            delete newState[action.bizId]
-            return newState
+            delete newState[action.bizId];
+            return newState;
     }
 }
---
+++ react-app/src/store/menu.js
@@ -0,0 +1,48 @@
+const LOAD_MENU = "/biz/LOAD_MENU";
+const ADD_TO_MENU = "/biz/ADD_TO_MENU";
+
+const loadMenu = (menu) => ({
+    type: LOAD_MENU,
+    menu,
+});
+
+const addMenu = (menu) => ({
+    type: ADD_TO_MENU,
+    menu,
+});
+
+export const getMenu = (menuId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/biz/${menuId}/menu`);
+    if (response.ok) {
+        const menu = await response.json();
+        dispatch(loadMenu(menu));
+    }
+};
+
+export const addMenuItem = (id, item) => async (dispatch) => {
+    const response = await csrfFetch(`/api/biz/${id}/menu`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(item),
+    });
+    if (response.ok) {
+        const menu = await response.json();
+        dispatch(addMenu(menu));
+    }
+};
+
+const menuReducer = (state = {}, action) => {
+    const newState = { ...state };
+
+    switch (action.type) {
+        case LOAD_MENU:
+            return action.menu;
+        case ADD_TO_MENU:
+            newState[action.menu.id] = action.menu;
+            return newState;
+    }
+};
+
+export default menuReducer;
