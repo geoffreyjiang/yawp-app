936c0daed0d57fc77ac6733238174e8c80bf2a73
geoffreyjiang <geoffreychjiang@gmail.com>
2023-01-04 19:18:56 -0800

    questions model done

---
+++ app/models/questions.py
@@ -0,0 +1,24 @@
+from .db import db, environment, SCHEMA
+
+class Question(db.Question):
+        __tablename__ = 'question'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id= db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey("user.id"), nullable=False)
+    business_id =  db.Column(db.Integer, ForeignKey("businesses.id"), nullable=False)
+    body = db.Column(db.String(280), nullable=False)
+
+    question_owner = db.relationship("User", back_populates="questions")
+    biz_question = db.relationship("Business", back_populates="question_biz")
+    question_answer = db.relationship("Answer", back_populates="answer_question")
+
+def to_dict(self):
+    return {
+        "id": self.id,
+        "user_id": self.user_id,
+        "business_id": self.business_id,
+        "body": self.body
+    }
---
+++ app/models/reviews.py
@@ -0,0 +1,28 @@
+from .db import db, environment, SCHEMA
+
+class Review(db.Review):
+    __tablename__ = 'reviews'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    business_id =  db.Column(db.Integer, ForeignKey("businesses.id"), nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey("users.id"), nullable=False)
+    image = db.Column(db.Image)
+    rating = db.Column(db.Integer, nullable=False)
+    body = db.Column(db.String(280), nullable=False)
+
+    review_biz = db.relationship('Business', back_populates="biz_review")
+    review_owner = db.relationship("User", back_populates="reviews")
+
+
+def to_dict(self):
+    return {
+        "id": self.id,
+        "business_id": self.business_id,
+        "user_id": self.user_id,
+        "image": self.image,
+        "rating": self.rating,
+        "body": self.body
+    }
