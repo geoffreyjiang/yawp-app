009a3c176819288f8f04dfee98ced3e6de47f337
Michael Love <michael.love42@gmail.com>
2023-01-04 20:25:16 -0800

    updates to redux preview store

--- redux_store_preview.js
+++ redux_store_preview.js
@@ -1,14 +1,19 @@
 const store = {
     biz: {
-        address1: "625 Hammy Driver",
-        address2: "345",
-        city: "The Bay",
-        id: 2,
-        image: "https://logos-download.com/wp-content/uploads/2016/04/Burger_King_logo_emblem-2.png",
-        name: "Burger King",
-        state: "New York",
-        userId: 2,
-        AvgRating: 4.7,
+        1: {
+            address1: "625 Hammy Driver",
+            address2: "345",
+            city: "The Bay",
+            id: 2,
+            image: "https://logos-download.com/wp-content/uploads/2016/04/Burger_King_logo_emblem-2.png",
+            name: "Burger King",
+            state: "New York",
+            userId: 2,
+            AvgRating: 4.7,
+            // list of questions (number of questions)
+            // list food of food items (ids)
+            // list of food images
+        },
     },
     questions: {
         2: {
@@ -17,7 +22,7 @@ const store = {
             id: 2,
             userId: 1,
             answers: {
-                3: { id: 3, body: "Thank you?", userId, username: "Demo" },
+                3: { id: 3, body: "Yes!", userId: 3, username: "Demo" },
             },
         },
     },
@@ -28,6 +33,7 @@ const store = {
             id: 4,
             image: "pic.jpg",
             stars: 4,
+            userId: 3,
         },
     },
     food: {
@@ -38,7 +44,7 @@ const store = {
             name: "Tacos",
             image: "pic2.jpg",
         },
-        1: {
+        2: {
             id: 2,
             businessId: 2,
             price: 3.6,
