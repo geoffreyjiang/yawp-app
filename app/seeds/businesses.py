6ed3f7c0ba4905961885664e6434c642e030f5c5
Michael Love <michael.love42@gmail.com>
2023-01-04 21:24:47 -0800

    seeded business and menus tables

---
+++ app/seeds/businesses.py
@@ -0,0 +1,50 @@
+from app.models import db, Business, environment, SCHEMA
+
+
+
+def seed_business():
+    biz1 = Business(
+        name="McDonalds", 
+        address1="3501 Grand Ave",
+        address2='',
+        city="LA",
+        state="California",
+        image="https://upload.wikimedia.org/wikinews/en/9/97/Mcdonalds-logo.JPG",
+        user_id=1
+
+        )
+    biz2 = Business(
+        name="Burger King", 
+        address1="625 Hammy Driver",
+        address2='345',
+        city="The Bay",
+        state="New York",
+        image="https://logos-download.com/wp-content/uploads/2016/04/Burger_King_logo_emblem-2.png",
+        user_id=2
+        )
+    
+    biz3 = Business(
+        name="Rally's", 
+        address1="123 University Road",
+        address2=423,
+        city="Seattle",
+        state="Washington",
+        image="https://as1.ftcdn.net/v2/jpg/04/57/56/50/1000_F_457565028_SNYUm8wEhR5vHgUyXDeZYAMYNS9PN4Ch.jpg",
+        user_id=3
+        )
+    
+    db.session.add(biz1)
+    db.session.add(biz2)
+    db.session.add(biz3)
+    db.session.commit()
+
+def undo_business():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.businesses RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM businesses")
+       
+
+        
+        
+    db.session.commit()
---
+++ app/seeds/menus.py
@@ -0,0 +1,59 @@
+from app.models import db, Menu, environment, SCHEMA
+
+
+
+def seed_answer():
+    item1 = Menu(
+        name="Hamburger",
+        price= 5.95,
+        business_id=1
+        )
+     
+    item2 = Menu(
+        name="French Fries",
+        price= 2.34,
+        business_id=1
+        )
+
+    item3 = Menu(
+        name="Ribs",
+        price= 21.99,
+        business_id=2
+        )
+    item4 = Menu(
+        name="Potatos",
+        price= 4.55,
+        business_id=2
+        )
+    item5 = Menu(
+        name="Lobster",
+        price= 134.55,
+        business_id=3
+        )
+    item6 = Menu(
+        name="Milk Shake",
+        price= 2.45,
+        business_id=3
+        )
+    
+
+    
+  
+    
+    db.session.add(item1)
+    db.session.add(item2)
+    db.session.add(item3)
+    db.session.add(item4)
+    db.session.add(item5)
+    db.session.add(item6)
+    
+    db.session.commit()
+
+def undo_menus():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.menus RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM menus")
+        
+        
+    db.session.commit()
