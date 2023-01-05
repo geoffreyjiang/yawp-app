f0eccb20e84bcead6373d0c80e79f23d02abdae7
Michael Love <michael.love42@gmail.com>
2023-01-04 19:52:53 -0800

    redux store preview

---
+++ redux_store_preview.js
@@ -0,0 +1,49 @@
+const store = {
+    biz: {
+        address1: "625 Hammy Driver",
+        address2: "345",
+        city: "The Bay",
+        id: 2,
+        image: "https://logos-download.com/wp-content/uploads/2016/04/Burger_King_logo_emblem-2.png",
+        name: "Burger King",
+        state: "New York",
+        userId: 2,
+        AvgRating: 4.7,
+    },
+    questions: {
+        2: {
+            body: "Does this place have dine in?",
+            businessId: 2,
+            id: 2,
+            userId: 1,
+            answers: {
+                3: { id: 3, body: "Thank you?", userId, username: "Demo" },
+            },
+        },
+    },
+    reviews: {
+        1: {
+            body: "this place is great",
+            businessId: 1,
+            id: 4,
+            image: "pic.jpg",
+            stars: 4,
+        },
+    },
+    food: {
+        1: {
+            id: 1,
+            businessId: 2,
+            price: 3.6,
+            name: "Tacos",
+            image: "pic2.jpg",
+        },
+        1: {
+            id: 2,
+            businessId: 2,
+            price: 3.6,
+            name: "Hotdogs",
+            image: "pic3.jpg",
+        },
+    },
+};
