1b77b2f8a8acc1c94df7b9eb4b298e25bca8b724
Michael Love <michael.love42@gmail.com>
2023-01-07 14:40:13 -0800

    home page first draft

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -1,17 +1,56 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getBusinesses } from "../../store/business";
+import { useHistory } from "react-router-dom";
+import { getBusinesses, getBusinessId } from "../../store/business";
+import { getQuestions } from "../../store/questions";
 import { getSelectedBizReviews } from "../../store/reviews";
-
+import "./index.css";
 const AllBiz = () => {
-    const biz = useSelector((store) => console.log(store));
-    console.log(biz);
+    const history = useHistory();
     const dispatch = useDispatch();
+    const business = useSelector((store) => {
+        return Object.values(store.business);
+    });
 
     useEffect(() => {
         dispatch(getBusinesses());
-    });
-    return <h1>we are here</h1>;
+    }, [dispatch]);
+
+    return (
+        <>
+            <div className="biz-preview">
+                {business.map((biz) => (
+                    <div className="biz-container">
+                        <div className="image-container">
+                            <img src={biz.image} alt={biz.name} />
+                        </div>
+                        <div className="biz-content">
+                            <div className="bizName">{biz.name}</div>
+                            <div>
+                                Location: {biz.city}, {biz.state}{" "}
+                            </div>
+                            <div className="review-details">
+                                <div>Average Review: {biz.averageRating}</div>
+                                <div>
+                                    Number of Reviews: {biz.numberOfReviews}
+                                </div>
+                            </div>
+                            <button
+                                onClick={() => {
+                                    history.push(`/biz/${biz.id}`);
+                                    dispatch(getBusinessId(biz.id));
+                                    dispatch(getSelectedBizReviews(biz.id));
+                                    dispatch(getQuestions(biz.id));
+                                }}
+                            >
+                                View Business
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default AllBiz;
