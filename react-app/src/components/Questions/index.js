c4e660f2c2103bd860cca477d163a55a8bd17a0e
geoffreyjiang <geoffreychjiang@gmail.com>
2023-01-08 20:33:39 -0800

    styled question container

--- react-app/src/components/Questions/index.js
+++ react-app/src/components/Questions/index.js
@@ -1,12 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link, useHistory } from "react-router-dom";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 // import { getBusinesses } from "../../store/business";
 import { getQuestions } from "../../store/questions";
+
 import "./questions.css";
 const AllQuestions = () => {
     const { bizId } = useParams();
     const dispatch = useDispatch();
+    const history = useHistory();
     const q = useSelector((store) => {
         return Object.values(store.questions);
     });
@@ -18,16 +20,19 @@ const AllQuestions = () => {
     const allQuestions = q.map((questions) => {
         return (
             <div className="question-item">
-                <div className="question-body-container">
-                    <h4>{questions.body}</h4>
-                </div>
+                <h4>{questions.body}</h4>
+                <Link to={`/biz/${bizId}/question`}>Answer this question</Link>
             </div>
         );
     });
-    // console.log(biz.id, "QUESTIONS COMPONENT");
     return (
         <>
-            <div className="questions-container">{allQuestions}</div>
+            <div className="questions-container">
+                {allQuestions}
+                <button onClick={() => history.push(`/biz/${bizId}/ask`)}>
+                    Ask a question
+                </button>
+            </div>
         </>
     );
 };
