bfbcf43375107c0c0b6d0c503c34610bb1068b8e
Michael Love <michael.love42@gmail.com>
2023-01-14 12:21:07 -0800

    updates review compoent with review cards

--- react-app/src/components/ReviewForm/index.js
+++ react-app/src/components/ReviewForm/index.js
@@ -1,24 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
-import businessReducer, { getBusinessId } from "../../store/business";
+import { getBusinessId } from "../../store/business";
 import { getSelectedBizReviews } from "../../store/reviews";
-import PostReview from "./CreateReview";
-
 import "./index.css";
 import Rating from "../Home/Rating";
 
 const ReviewForm = () => {
-    const [userHasReview, setUserHasReview] = useState(false);
     const history = useHistory();
     const dispatch = useDispatch();
     const { bizId } = useParams();
-    const biz = useSelector((state) => state.business);
+
     const sessionUser = useSelector((state) => state.session.user);
-    console.log(biz, "<=== Biz");
+
     const bizReviews = useSelector((state) => {
         return Object.values(state.reviews);
     });
+    const review = bizReviews.map((item) => {
+        return item;
+    });
+
+    const user = useSelector((state) => state.session.user);
+
     const userReview = bizReviews?.filter(
         (review) => review?.userId === sessionUser?.id
     );
@@ -28,41 +31,13 @@ const ReviewForm = () => {
         dispatch(getSelectedBizReviews(bizId));
     }, [dispatch, bizId]);
 
-    console.log(bizReviews[0], "review");
-    console.log(userReview, "<==== USER REVIEW");
     return (
-        <div>
-            <h1>Reviews</h1>
-            {userReview.length ? (
-                <button
-                    onClick={() => {
-                        history.push(
-                            `/biz/${bizId}/reviews/${userReview[0].id}`
-                        );
-                    }}
-                >
-                    Update Review
-                </button>
-            ) : (
-                <button
-                    onClick={() => {
-                        history.push(`/biz/${bizId}/reviews`);
-                        <PostReview user={sessionUser} />;
-                    }}
-                >
-                    Create Review
-                </button>
-            )}
-            {/* {bizReviews?.map((review) => {
-                if (review?.userId === sessionUser?.id) {
-                    return (
-                        <button>
-                            Update Review
-                        </button>
-                    )
-                }
-                else {
-                    return (
+        <>
+            <div className="header-review">
+                <h1>Review Section</h1>
+
+                <div className="review-div">
+                    {userReview.length == 0 ? (
                         <button
                             onClick={() => {
                                 history.push(`/biz/${bizId}/reviews`);
@@ -70,21 +45,56 @@ const ReviewForm = () => {
                         >
                             Write A Review
                         </button>
-                    )
-                }
-            })} */}
-            <div className="reviewsHolder">
-                {bizReviews?.map((review) => {
-                    return (
-                        <div class="reviews" key={review?.id}>
-                            <p className="reviewBody">{review?.body}</p>
-                            <h2 className="rating">{review?.rating}</h2>
-                            <h4 className="reviewUser">{review?.firstName}</h4>
-                        </div>
-                    );
-                })}
+                    ) : null}
+                </div>
             </div>
-        </div>
+            <section className="reviews">
+                <div className="reviews-box-container">
+                    {bizReviews?.map((review) => (
+                        <>
+                            <div className="review-box">
+                                <div className="box-top">
+                                    <div className="profile">
+                                        <div className="name-user">
+                                            <strong>
+                                                {review.firstName}{" "}
+                                                {review.lastName}
+                                            </strong>
+                                        </div>
+                                    </div>
+
+                                    <div className="reviewsT">
+                                        <i>
+                                            <Rating value={review?.rating} />{" "}
+                                        </i>
+                                    </div>
+                                </div>
+                                <div className="comment-section">
+                                    <div className="client-comment">
+                                        <p>{review?.body}</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    {user.id == review.userId ? (
+                                        <>
+                                            <button
+                                                onClick={() => {
+                                                    history.push(
+                                                        `/biz/${bizId}/reviews/${userReview[0].id}`
+                                                    );
+                                                }}
+                                            >
+                                                Update Review
+                                            </button>
+                                        </>
+                                    ) : null}
+                                </div>
+                            </div>
+                        </>
+                    ))}
+                </div>
+            </section>
+        </>
     );
 };
 
