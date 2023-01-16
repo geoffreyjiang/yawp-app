233939d587be33852d443140e2268801353752fd
Andre <andre.christian.silva@gmail.com>
2023-01-15 22:48:50 -0800

    Fixed edit reviews.

--- react-app/src/components/ReviewForm/EditReview.js
+++ react-app/src/components/ReviewForm/EditReview.js
@@ -5,20 +5,22 @@ import { addReview } from "../../store/reviews";
 import { getBusinesses } from "../../store/business";
 import { editMyReview } from "../../store/reviews";
 import { deleteMyReview } from "../../store/reviews";
-
+import { getSelectedBizReviews } from "../../store/reviews";
+import './EditReview.css'
 function EditReview() {
     const history = useHistory();
     const dispatch = useDispatch();
-    const sessionUser = useSelector((state) => state.session.user);
+    const sessionUser = useSelector((state) => state?.session.user);
     const { bizId } = useParams();
     const { reviewId } = useParams();
-    const review = useSelector((state) => state.reviews[reviewId]);
-    const [body, setBody] = useState(review.body);
-    const [rating, setRating] = useState(review.rating);
+    const review = useSelector((state) => state?.reviews[reviewId]);
+    const [body, setBody] = useState(review?.body);
+    const [rating, setRating] = useState(review?.rating);
     const [userId, setUserId] = useState(sessionUser?.id);
 
     useEffect(() => {
         dispatch(getBusinesses());
+        dispatch(getSelectedBizReviews(bizId))
     }, [dispatch]);
 
     const handleSubmit = async (e) => {
@@ -45,7 +47,7 @@ function EditReview() {
     };
 
     const deleteReview = (e) => {
-        dispatch(deleteMyReview(review.id));
+        dispatch(deleteMyReview(review?.id));
         alert("Review successfully removed!");
         history.push(`/biz/${bizId}`);
     };
@@ -53,7 +55,7 @@ function EditReview() {
         <>
             <section className="edit-review">
                 <form className="update-review" onSubmit={handleSubmit}>
-                    <h3 className="review-text">New Review</h3>
+                    <h3 className="review-text">Edit Review</h3>
                     <input
                         className="review-range"
                         type="range"
